59f804cad3a220db815db72588ea8fbe41a43e10
Enrique Rodriguez <enrique.rodriguez@nazaries.com>
2018-11-27 09:30:45 +0100

    Exercise finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,36 @@ class User < ApplicationRecord
     under_stock_limit? && !stock_already_added?(ticker_symbol)
   end
 
+  def self.search(param)
+    param.strip!
+    param.downcase!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.first_name_matches(params)
+    matches('first_name', params)
+  end
+
+  def self.last_name_matches(params)
+    matches('last_name', params)
+  end
+
+  def self.email_matches(params)
+    matches('email', params)
+  end
+
+  def self.matches(field_name, params)
+    User.where("#{field_name} like ?", "%#{params}%")
+  end
+
+  def except_current_user(users)
+    users.reject {|user| user.id == self.id }
+  end
+
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1
+  end
+
 end
